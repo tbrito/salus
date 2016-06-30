089f7d43633f308c2b296c84b45b8f2e00ba6b80
tbrito <tiago.sousa.brito@gmail.com>
2016-06-29 23:25:31 -0300

    ElasticSearch

---
+++ src/Salus.Infra/ElasticSearch/ElasticSearchIndex.cs
@@ -0,0 +1,18 @@
+﻿using Nest;
+using Salus.Model.Entidades;
+using System;
+
+namespace Salus.Infra.ElasticSearch
+{
+    public class ElasticSearchIndex
+    {
+        public void Indexar(SearchObject indexacao)
+        {
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+
+            client.Index(indexacao);
+        }
+    }
+}
